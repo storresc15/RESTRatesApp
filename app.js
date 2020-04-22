f9d153983e06b4bd92e92d566afb931d913d1fe3
Sebastian Torres <sebas.torres.88@gmail.com>
2020-04-22 02:45:30 +0000

    Added Sanitizer

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 var express = require("express"),
 	app = express(),
 	request = require("request"),
-	bodyParser = require("body-parser");
+	bodyParser = require("body-parser"),
+	expressSanitizer = require('express-sanitizer');
 const PORT = process.env.PORT || 3000;
 
 //Test using the body parser 
@@ -12,13 +13,14 @@ app.use(bodyParser.json());
 //Rest of the settings
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
+app.use(expressSanitizer());
 
 app.get("/", function(req, res) {
 	res.render("getRate");
 });
 
 app.get("/results", function(req, res){
-    var query = req.query.amount;
+    var query = req.sanitize(req.query.amount);
 	// Don't use this: var userInput = req.body.amount;
 	console.log("This is the value of query.amount: " + query);
     var url = "http://data.fixer.io/api/latest?access_key=8a31270dcf21bc8993b57ea28f81c9ce&symbols=USD,CAD,EUR,MXN";
